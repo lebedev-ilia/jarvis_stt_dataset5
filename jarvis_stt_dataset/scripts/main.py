6083c317f088796c28bedaa884790b68b801e630
lebedev-ilia <150586009+lebedev-ilia@users.noreply.github.com>
2025-03-16 14:46:48 +1000

    Update main.py

--- jarvis_stt_dataset/scripts/main.py
+++ jarvis_stt_dataset/scripts/main.py
@@ -12,6 +12,8 @@ from configs.const import const
 
 parser = argparse.ArgumentParser(description='Create dataset')
 parser.add_argument("--data_root", required=True, default=None)
+parser.add_argument("--logs_path", required=False, default=None)
+parser.add_argument("--text_coding", required=False, default=True)
 parser.add_argument("--shuffle", required=False, default=True)
 parser.add_argument("--hf_api_key", required=True, default=False)
 args = parser.parse_args()
@@ -84,48 +86,10 @@ if 'mozilla-foundation' not in os.listdir(f'{main_path}/rus'):
 
 dist_path = f'{args.data_root}/{path2dist}'
 
-print(len(os.listdir(f'{dist_path}/test')) == 1)
-
 if len(os.listdir(f'{dist_path}/train')) == 1 or len(os.listdir(f'{dist_path}/validation')) == 1 or len(os.listdir(f'{dist_path}/test')) == 1:
     
     os.system(f'python3 {args.data_root}/{path2scripts}/process_to_manifest.py \
         --data_root={args.data_root} \
-        --main_ratio={args.main_ratio} \
-        --dist_ratio={args.dist_ratio} \
+        --logs_path={args.logs_path} \
+        --text_coding={args.text_coding} \
         --shuffle={args.shuffle}')
-
-if 'tokenizer' not in os.listdir(f'{args.data_root}/jarvis_stt_dataset/jstt_dataset'):
-    
-    MANIFEST_CLEANED = f'{args.data_root}/{path2dist}'
-    VAL_MANIFEST_CLEANED = f"{MANIFEST_CLEANED}/validation/jarvis_stt_validation_manifest.json"
-    TRAIN_MANIFEST_CLEANED = f"{MANIFEST_CLEANED}/train/jarvis_stt_train_manifest.json"
-    TOKENIZER_DIR = f"{args.data_root}/jarvis_stt_dataset/jstt_dataset/tokenizer"
-    
-    def get_charset(manifest_data):
-        charset = defaultdict(int)
-        for row in tqdm(manifest_data, desc="Computing character set"):
-            text = row['text']
-            for character in text:
-                charset[character] += 1
-        return charset
-
-    val_manifest_data = read_manifest(VAL_MANIFEST_CLEANED)
-    train_manifest_data = read_manifest(TRAIN_MANIFEST_CLEANED)
-
-    val_charset = get_charset(val_manifest_data)
-    train_charset = get_charset(train_manifest_data)
-
-    train_val_set = set.union(set(val_charset.keys()), set(train_charset.keys()))
-
-    VOCAB_SIZE = len(train_val_set) + 2
-
-    os.system(f'python3 {args.data_root}/{path2scripts}/process_asr_text_tokenizer.py \
-    --manifest={VAL_MANIFEST_CLEANED},{TRAIN_MANIFEST_CLEANED} \
-    --vocab_size={VOCAB_SIZE} \
-    --data_root={TOKENIZER_DIR} \
-    --tokenizer="spe" \
-    --spe_type="bpe" \
-    --spe_character_coverage=1.0 \
-    --no_lower_case')
-
-
